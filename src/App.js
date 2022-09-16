a5a58e3f3f201e87017779aabc31f06d148bdc8b
Siddhesh Surve <sysurve24@gmail.com>
2022-09-16 03:46:55 +0000

    added css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from "react";
-
+import Contact from "./Contact";
+import Whis from "./images/mr-whiskerson.png"
+import Fluff from "./images/fluffykins.png"
+import Felix from "./images/felix.png"
+import Pump from "./images/pumpkin.png"
+import "./index.css"
 export default function App () {
   return (
     <>
-      
+      <div className="contacts">
+        <Contact
+            img = {Whis}
+            name="Mr. Whiskerson"
+            phone="(212) 555-1234"
+            email="mr.whiskaz@catnap.meow"
+        />
+        <Contact 
+            img= {Fluff}
+            name="Fluffykins"
+            phone="(212) 555-2345"
+            email="fluff@me.com"
+        />
+        <Contact 
+            img= {Felix}
+            name="Felix"
+            phone="(212) 555-4567"
+            email="thecat@hotmail.com"
+        />
+        <Contact 
+            img= {Pump}
+            name="Pumpkin"
+            phone="(0800) CAT KING"
+            email="pumpkin@scrimba.com"
+        />
+      </div>
     </>
   )
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import PhoneIcon from "./images/phone-icon.png"
+import MailIcon from "./images/mail-icon.png"
+import "./index.css"
 export default function Contact (props) {
     return (
         <>
@@ -7,11 +9,11 @@ export default function Contact (props) {
                 <img src={props.img} />
                 <h3>{props.name}</h3>
                 <div className="info-group">
-                    <img src="./images/phone-icon.png" />
+                    <img src= {PhoneIcon} />
                     <p>{props.phone}</p>
                 </div>
                 <div className="info-group">
-                    <img src="./images/mail-icon.png" />
+                    <img src= {MailIcon} />
                     <p>{props.email}</p>
                 </div>
              </div>
