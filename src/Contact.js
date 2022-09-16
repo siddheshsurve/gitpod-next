be4b10cc3d0f2659d8c8cfe934d575800a3827ce
Siddhesh Surve <sysurve24@gmail.com>
2022-09-16 03:25:10 +0000

    introduced contact component

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export default function Contact () {
+export default function Contact (props) {
     return (
         <>
-             
+             <div className="contact-card">
+                <img src={props.img} />
+                <h3>{props.name}</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>{props.phone}</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>{props.email}</p>
+                </div>
+             </div>
         </>
     )
 }
